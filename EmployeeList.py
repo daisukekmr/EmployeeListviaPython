d243ab392e9c69f369770e44b5d7e79cc77598b7
Daisuke Kimura <yakotonringokuu@gmail.com>
2015-02-22 18:43:19 +0900

    削除の検索機能を実装 編集関数を登録と削除関数によって構成 各入力項目のエラーチェックを実装

--- EmployeeList.py
+++ EmployeeList.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import re
 
 class employee:
 
@@ -9,40 +10,53 @@ class employee:
         self.bd = bd
         self.salary = salary
 
-#
-"""
-class name:
 
-    def __init__(self,id,name):
-        self.id = id
-        self.name = name
+class p_bd:
 
-class bd:
+    def __init__(self,year,month,date):
+        self.year = year
+        self.month = month
+        self.date = date
 
-    def __init__(self,id,bd):
-        self.id = id
-        self.bd = bd
-
-class salary:
-
-    def __init__(self,id,salary):
-        self.id = id
-        self.salary = salary
-"""
 
 def register(employeeid):
 
     print "名前を入力してください"
     tempname = raw_input()
-    print "生年月日を入力してください"
-    tempbd = raw_input()
-    print "月額の給与を入力してください"
-    tempsalary = raw_input()
-    EmployeeList.append(employee(employeeid,tempname,tempbd,tempsalary))
+    print "生年月日を入力してください(yyyy/mm/ddの形で)"
+    while(1):
+        tempbd = raw_input()
+        bdlist = tempbd.split("/")
+        if len(bdlist) != 3:
+            print "不正な値です．再度入力してください"
+        elif int(bdlist[0]) > 2015:
+            print "不正な値です．再度入力してください"
+        elif int(bdlist[1]) > 12 or int(bdlist[1]) < 1:
+            print "不正な値です．再度入力してください"
+        elif int(bdlist[2]) > 31 or int(bdlist[2]) < 1:
+            print "不正な値です．再度入力してください"
+        else:
+            break
+    bdclass = p_bd(bdlist[0],bdlist[1],bdlist[2])
+
+    print "月額の給与を入力してください(8桁まで)"
+    while(1):
+        tempsalary = raw_input()
+        if tempsalary.isdigit():
+            tempintsalary = int(tempsalary)
+            if tempintsalary <= 99999999:
+                break
+            else:
+                print "桁数が多すぎます．再度入力してください"
+        else:
+            print "数値ではありません．再度入力してください"
+
+    EmployeeList.append(employee(employeeid,tempname,bdclass,tempintsalary))
 
     employeeidtemp = employeeid + 1
 
     return(employeeidtemp)
+
 
 def intro():
 
@@ -50,37 +64,39 @@ def intro():
     print "ID   名前       生年月日     給与"
     print "*****************************"
     for i in EmployeeList:
-#        strid = str(i.id)
-        string = str(i.id) + "   " + i.name + "    " + i.bd + "      " + i.salary
+        string = str(i.id) + "   " + i.name + "    " + i.bd.year + "年" + i.bd.month +"月" + i.bd.date + "日" + "      " + '{:,d}'.format(i.salary) + "円"
         print string
     print "*****************************"
 
 
-def delete():
 
-    print "削除する社員のIDを入力してください"
-    deleteid = input() - 1
+def delete(requiredid):
 
-    deletedlist = EmployeeList.pop(deleteid)
-    print "%sを削除しました" % deletedlist.name
+    j = 0
+    if requiredid == 0:
+        print "削除する社員のIDを入力してください"
+        deleteid = input()
+
+    else:
+        deleteid = requiredid
+
+    for deletenum in EmployeeList:
+        if deletenum.id == deleteid:
+            deletedList = EmployeeList.pop(j)
+            if requiredid == 0:
+                print "%sを削除しました" % deletedList.name
+            break
+        j = j + 1
 
 
 def edit():
 
     print "更新したい社員のIDを入力してください"
     editid = input()
-    index = editid - 1
 
-    print "新しい情報を入力してください"
-    print "名前を入力してください"
-    tempname = raw_input()
-    print "生年月日を入力してください"
-    tempbd = raw_input()
-    print "月額の給与を入力してください"
-    tempsalary = raw_input()
+    delete(editid)
+    register(editid)
 
-    EmployeeList.pop(index)
-    EmployeeList.insert(index,employee(editid,tempname,tempbd,tempsalary))
 
 
 
@@ -105,7 +121,7 @@ if __name__ == "__main__":
         elif select == "2":
             intro()
         elif select == "3":
-            delete()
+            delete(0)
         elif select == "4":
             edit()
         else:
